4dbdb644489cafc13e51f3be82985c56503f0aaa
qwe916 <qwe916@naver.com>
2023-01-04 16:23:39 +0900

    BaseTime 생성

---
+++ LuckyBag-BackEnd/src/main/java/com/luckybag/luckybagbackend/domain/BaseTime.java
@@ -0,0 +1,27 @@
+package com.luckybag.luckybagbackend.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseTime {
+
+    @CreatedDate
+    @Column(updatable = false)
+    protected LocalDateTime createDate;
+
+    @LastModifiedDate
+    protected LocalDateTime lastModifiedDate;
+
+}
