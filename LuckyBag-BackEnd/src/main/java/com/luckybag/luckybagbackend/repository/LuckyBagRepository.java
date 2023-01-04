86ee804b62c3893c98841c4308e0293a4297f0d3
qwe916 <qwe916@naver.com>
2023-01-04 19:19:42 +0900

    fix : MemberId로 LuckyBag 찾기

--- LuckyBag-BackEnd/src/main/java/com/luckybag/luckybagbackend/repository/LuckyBagRepository.java
+++ LuckyBag-BackEnd/src/main/java/com/luckybag/luckybagbackend/repository/LuckyBagRepository.java
@@ -3,5 +3,9 @@ package com.luckybag.luckybagbackend.repository;
 import com.luckybag.luckybagbackend.domain.LuckyBag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LuckyBagRepository extends JpaRepository<LuckyBag,Long> {
+import java.util.List;
+
+public interface LuckyBagRepository extends JpaRepository<LuckyBag, Long> {
+    LuckyBag findByMemberId(String memberId);
+
 }
