c9858af3cfeb8e8c75ba43ef783170b0cdcb4c45
qwe916 <qwe916@naver.com>
2023-01-04 17:42:03 +0900

    feat : BaseTime.java

--- LuckyBag-BackEnd/src/main/java/com/luckybag/luckybagbackend/domain/BaseTime.java
+++ LuckyBag-BackEnd/src/main/java/com/luckybag/luckybagbackend/domain/BaseTime.java
@@ -6,22 +6,17 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
-
 
 @Getter
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public abstract class BaseTime {
-
     @CreatedDate
     @Column(updatable = false)
     protected LocalDateTime createDate;
-
     @LastModifiedDate
     protected LocalDateTime lastModifiedDate;
-
 }
