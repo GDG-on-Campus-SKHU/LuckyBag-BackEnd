49cafea95b1dbd985a276b068c5a11c404c6f3bf
김자경 <rlawkrud15@gmail.com>
2023-01-04 15:45:17 +0900

    feat : 패키지 생성

---
+++ LuckyBag-BackEnd/src/main/java/com/luckybag/luckybagbackend/LuckyBagBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.luckybag.luckybagbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckyBagBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckyBagBackEndApplication.class, args);
+    }
+
+}
