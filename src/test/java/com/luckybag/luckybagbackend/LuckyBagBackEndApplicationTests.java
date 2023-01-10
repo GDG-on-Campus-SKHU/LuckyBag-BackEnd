d02a8d051339616cec789aa3fbf366c871683062
jkkj0414 <rlawkrud15@gmail.com>
2023-01-10 21:10:45 +0900

    Update LuckyBagBackEndApplicationTests.java

--- src/test/java/com/luckybag/luckybagbackend/LuckyBagBackEndApplicationTests.java
+++ src/test/java/com/luckybag/luckybagbackend/LuckyBagBackEndApplicationTests.java
@@ -3,7 +3,7 @@ package com.luckybag.luckybagbackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class LuckyBagBackEndApplicationTests {
 
     @Test
