5c05bd1a5ef4d25d4d11c4eaf5c9f2d957c1f0c3
Hong <77445491+qwe916@users.noreply.github.com>
2023-01-11 18:55:09 +0900

    fix : { 수정

--- src/main/java/com/luckybag/luckybagbackend/login/config/RedisRepositoryConfig.java
+++ src/main/java/com/luckybag/luckybagbackend/login/config/RedisRepositoryConfig.java
@@ -26,7 +26,7 @@ public class RedisRepositoryConfig {
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(redisHost, redisPort);
-
+    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
