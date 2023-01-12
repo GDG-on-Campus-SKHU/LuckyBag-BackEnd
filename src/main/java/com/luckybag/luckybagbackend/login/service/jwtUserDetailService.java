3d63b92b87d928b82dbf239f9938f37e9f5165c9
qwe916 <qwe916@naver.com>
2023-01-12 23:45:17 +0900

    fix : log

--- src/main/java/com/luckybag/luckybagbackend/login/service/jwtUserDetailService.java
+++ src/main/java/com/luckybag/luckybagbackend/login/service/jwtUserDetailService.java
@@ -3,6 +3,7 @@ package com.luckybag.luckybagbackend.login.service;
 import com.luckybag.luckybagbackend.domain.Member;
 import com.luckybag.luckybagbackend.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,12 +11,14 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class jwtUserDetailService implements UserDetailsService {
     private final MemberRepository memberRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("loadUserByUsername");
         return memberRepository.findByMemberId(username)
                 .map(this::createUserDetails)
                 .orElseThrow(() -> new UsernameNotFoundException("해당하는 유저를 찾을 수 없습니다."));
