f7c71b0eb1373cfd68ed28763b8e974f7f330884
qwe916 <qwe916@naver.com>
2023-01-12 13:25:30 +0900

    feat : CORS

--- src/main/java/com/luckybag/luckybagbackend/LuckyBagBackEndApplication.java
+++ src/main/java/com/luckybag/luckybagbackend/LuckyBagBackEndApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class LuckyBagBackEndApplication {
--- src/main/java/com/luckybag/luckybagbackend/controller/ServiceController.java
+++ src/main/java/com/luckybag/luckybagbackend/controller/ServiceController.java
@@ -10,11 +10,14 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-// [Get] /luckybags -> 모두 조회
-// [Get] /luckybags/{id} -> 복주머니 리스트에서 하나만 조회
-// [Post] /luckybags/{id} -> 저장
-// [patch] /luckybas/{id} -> 수정
-// [delete] /luckybags/{id} -> 삭제
+// [GET] http://luckybags.duckdns.org/luckybags -> 모두 조회
+// [GET] http://luckybags.duckdns.org/luckybags/{id} -> 복주머니 리스트에서 하나만 조회
+// [POST] http://luckybags.duckdns.org/luckybags/{id} -> 저장
+// [PATCH] http://luckybags.duckdns.org/luckybas/{id} -> 수정
+// [DELETE] http://luckybags.duckdns.org/luckybags/{id} -> 삭제
+// [POST] http://luckybags.duckdns.org/signup
+// [POST] http://luckybags.duckdns.org/login
+// [POST] http://luckybags.duckdns.org/log-out
 @RestController
 @RequiredArgsConstructor
 public class ServiceController {
